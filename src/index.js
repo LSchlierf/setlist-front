463ce5883bfd3b5cf65137a85945a8a3e184a79a
Lucas Schlierf <lucas.schlierf@tum.de>
2024-04-09 23:26:24 +0200

    index fix

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EditRepertoire from './EditRepertoire';
 import EditSetlist from './EditSetlist';
-import { PDFViewer } from '@react-pdf/renderer';
-import SetlistDetailedPDF from './SetlistDetailedPDF';
-import storage from './storage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,11 +13,6 @@ root.render(
       <Route path='/' element={<App />} />
       <Route path='editRepertoire' element={<EditRepertoire />} />
       <Route path='editSetlist' element={<EditSetlist />} />
-      <Route path='test' element={
-        <PDFViewer width={'100%'} height={'939'}>
-          <SetlistDetailedPDF setlist={Object.values(storage.getSetlists())[0]} repertoire={storage.getRepertoire()} />
-        </PDFViewer>
-      } />
     </Routes>
   </BrowserRouter>
 );
