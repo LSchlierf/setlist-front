e93d486b8466270c7d629334bc9f1ea38db4f6c0
Lucas Schlierf <LucasSchlierf@gmail.com>
2025-09-15 22:35:08 +0200

    fixed account name display

--- src/Header.js
+++ src/Header.js
@@ -6,14 +6,16 @@ export default function Header(props) {
 
   return (
     <div className='header'>
-      <span style={{ width: 42, display: 'flex' }}>
-        {props.leftButton}
-      </span>
+      <div className='header-buttons'>
+        <span style={{ display: 'flex' }}>
+          {props.leftButton}
+        </span>
+        <span style={{ display: 'flex' }}>
+          {props.rightButton}
+        </span>
+      </div>
       <span className='title'>
         {props.title}
-      </span>
-      <span style={{ width: 42, display: 'flex' }}>
-        {props.rightButton}
       </span>
     </div>
   )
