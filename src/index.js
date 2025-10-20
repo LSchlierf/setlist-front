d997ba97eda88195dd34616c7eaf6d0adb9648bb
Lucas Schlierf <lucas.schlierf@tum.de>
2025-10-20 22:59:01 +0200

    changed pdf rendering scheme

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EditRepertoire from './EditRepertoire';
 import EditSetlist from './EditSetlist';
 import { PDFViewer } from '@react-pdf/renderer';
+import SetlistSimplePDF from './SetlistSimplePDF';
 import SetlistDetailedPDF from './SetlistDetailedPDF';
 import storage from './storage';
 
@@ -16,13 +17,11 @@ root.render(
       <Route path='/' element={<App />} />
       <Route path='editRepertoire' element={<EditRepertoire />} />
       <Route path='editSetlist' element={<EditSetlist />} />
-      <Route path='test' element={
+      {/* <Route path='test' element={
         <PDFViewer width={'100%'} height={'939'}>
-          <SetlistDetailedPDF setlist={Object.values(storage.getSetlists())[0]} repertoire={storage.getRepertoire()} />
+          <SetlistSimplePDF setlist={await storage.getSetlist(Object.values(await storage.getSetlists())[0].id)} repertoire={storage.getRepertoire()} />
         </PDFViewer>
-      } />
+      } /> */}
     </Routes>
   </BrowserRouter>
 );
-
-// Roadmap
